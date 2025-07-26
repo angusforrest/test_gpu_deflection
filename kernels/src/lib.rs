2975fb44c4033dc5962b220b466ef10c370aa366
Jack Patterson <jack.patterson.nz@gmail.com>
2025-07-27 11:25:40 +1200

    Add vecadd example including pulling crates directly from github

--- kernels/src/lib.rs
+++ kernels/src/lib.rs
@@ -1,7 +1,11 @@
-#![cfg_attr(
-    target_os = "cuda",
-    no_std,
-    register_attr(nvvm_internal)
-)]
+use cuda_std::prelude::*;
 
-use cuda_std::*;
+#[kernel]
+#[allow(improper_ctypes_definitions, clippy::missing_safety_doc)]
+pub unsafe fn vecadd(a: &[f32], b: &[f32], c: *mut f32) {
+    let idx = thread::index_1d() as usize;
+    if idx < a.len() {
+        let elem = unsafe { &mut *c.add(idx) };
+        *elem = a[idx] + b[idx];
+    }
+}
