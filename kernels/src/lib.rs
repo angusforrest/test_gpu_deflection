3d9e1d907447ba3d5d2d5c8e3b2cd7dd451560e9
Jack Patterson <jack.patterson.nz@gmail.com>
2025-07-27 11:50:07 +1200

    Grab gemm example

--- kernels/src/lib.rs
+++ kernels/src/lib.rs
@@ -1,11 +1,5 @@
-use cuda_std::prelude::*;
+mod gemm_naive;
+mod gemm_tiled;
 
-#[kernel]
-#[allow(improper_ctypes_definitions, clippy::missing_safety_doc)]
-pub unsafe fn vecadd(a: &[f32], b: &[f32], c: *mut f32) {
-    let idx = thread::index_1d() as usize;
-    if idx < a.len() {
-        let elem = unsafe { &mut *c.add(idx) };
-        *elem = a[idx] + b[idx];
-    }
-}
+pub use crate::gemm_naive::gemm_naive;
+pub use crate::gemm_tiled::gemm_tiled;
