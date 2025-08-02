8fbc2c8058e12f1a1d885f3ff83f9bda8ebf6595
Jack Patterson <jack.patterson.nz@gmail.com>
2025-08-03 10:12:51 +1200

    Calc 4th order and error estimate

--- kernels/src/dop_54.rs
+++ kernels/src/dop_54.rs
@@ -13,6 +13,34 @@ fn compute_acceleration(t: f32, x: f32, y: f32, z: f32) -> (f32, f32, f32) {
     (a * x, a * y, a * z)
 }
 
+#[inline(always)]
+pub fn rk_norm(
+    x: f32, x_new: f32, err_x: f32,
+    y: f32, y_new: f32, err_y: f32,
+    z: f32, z_new: f32, err_z: f32,
+    vx: f32, vx_new: f32, err_vx: f32,
+    vy: f32, vy_new: f32, err_vy: f32,
+    vz: f32, vz_new: f32, err_vz: f32,
+    atol: f32, rtol: f32
+) -> f32 {
+    let sc_x  = atol + rtol * f32::max(x.abs(), x_new.abs());
+    let sc_y  = atol + rtol * f32::max(y.abs(), y_new.abs());
+    let sc_z  = atol + rtol * f32::max(z.abs(), z_new.abs());
+    let sc_vx = atol + rtol * f32::max(vx.abs(), vx_new.abs());
+    let sc_vy = atol + rtol * f32::max(vy.abs(), vy_new.abs());
+    let sc_vz = atol + rtol * f32::max(vz.abs(), vz_new.abs());
+
+    // might need to guard against div by 0
+    let sum = (err_x / sc_x).powi(2)
+            + (err_y / sc_y).powi(2)
+            + (err_z / sc_z).powi(2)
+            + (err_vx / sc_vx).powi(2)
+            + (err_vy / sc_vy).powi(2)
+            + (err_vz / sc_vz).powi(2);
+
+    (sum / 6.0).sqrt()
+}
+
 #[kernel]
 #[allow(improper_ctypes_definitions)]
 pub unsafe fn dopr54_step(
@@ -22,6 +50,9 @@ pub unsafe fn dopr54_step(
     current_step: usize,
     dt: f32,
     t0: f32,
+    atol: f32,
+    rtol: f32,
+    error_out: *mut f32,
 ) {
     let tid = (thread::block_idx_x() * thread::block_dim_x() + thread::thread_idx_x()) as usize;
     if tid >= n {
@@ -98,7 +129,53 @@ pub unsafe fn dopr54_step(
         vz_new += dt * b * rk_vz[i];
     }
 
-    // note: because we aren't doing adaptive stepping this is DOP5 not DOP54
+    // now we've completed 5th order, we do 4th:
+
+    let mut x_hat = x;
+    let mut y_hat = y;
+    let mut z_hat = z;
+    let mut vx_hat = vx;
+    let mut vy_hat = vy;
+    let mut vz_hat = vz;
+
+    for i in 0..Coeffs::STAGES {
+        let b_hat = Coeffs::B_HAT[i];
+        x_hat += dt * b_hat * rk_x[i];
+        y_hat += dt * b_hat * rk_y[i];
+        z_hat += dt * b_hat * rk_z[i];
+        vx_hat += dt * b_hat * rk_vx[i];
+        vy_hat += dt * b_hat * rk_vy[i];
+        vz_hat += dt * b_hat * rk_vz[i];
+    }
+
+    // and compute truncation error est per component
+    let err_x = x_new - x_hat;
+    let err_y = y_new - y_hat;
+    let err_z = z_new - z_hat;
+    let err_vx = vx_new - vx_hat;
+    let err_vy = vy_new - vy_hat;
+    let err_vz = vz_new - vz_hat;
+
+    let rk_err = rk_norm(
+        x, x_new, err_x,
+        y, y_new, err_y,
+        z, z_new, err_z,
+        vx, vx_new, err_vx,
+        vy, vy_new, err_vy,
+        vz, vz_new, err_vz,
+        atol, rtol
+    );
+
+    if !error_out.is_null() {
+        *error_out.add(current_step * n + tid) = rk_err;
+    }
+
+    // later:
+    // if rk_err <= 1.0 {
+    // accept step
+    // } else {
+    // reject and reduce dt
+    // }
 
     *state_out.add(curr_offset + 0) = x_new;
     *state_out.add(curr_offset + 1) = y_new;
