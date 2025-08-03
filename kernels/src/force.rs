3b929a17e1e3b820b60a1b4e5cb478ce6bef2b80
angusforrest <contact@angusforrest.com>
2025-08-03 12:23:32 +1200

    changed force to return cartesian acceleration vector

--- kernels/src/force.rs
+++ kernels/src/force.rs
@@ -4,22 +4,49 @@ use libm::sqrtf;
 
 const PI: f32 = 3.14159265358979323846264338327950288;
 
+const AMP_S: f32 = 67168.3897826272;
+const AMP_MN: f32 = 0.7574802019;
+const AMP_NFW: f32 = 7.03716754404;
+
 #[inline(always)]
-fn sphericalcutoff_force(r: f32, amp: f32, alpha: f32, r1: f32, c2: f32) -> f32 {
-    let r2 = powf(r, 2);
-    -amp * (powf(r1 / r, alpha) * (alpha * c2 + 2 * r2) * expf(-r2 / c2)) / (r * c2)
+fn sphericalcutoff_force(
+    x: f32,
+    y: f32,
+    z: f32,
+    amp: f32,
+    alpha: f32,
+    r1: f32,
+    c2: f32,
+) -> (f32, f32, f32) {
+    let r2 = powf(x, 2) + powf(y, 2) + powf(z, 2);
+    let r = sqrtf(r2);
+    let a = -amp * (powf(r1 / r, alpha) * (alpha * c2 + 2 * r2) * expf(-r2 / c2)) / (r * c2);
+    let ax = a * (x / r);
+    let ay = a * (y / r);
+    let az = a * (z / r);
+    (ax, ay, az)
 }
-fn navarro_frenk_white_force(r: f32, amp: f32, a: f32) -> f32 {
+fn navarro_frenk_white_force(x: f32, y: f32, z: f32, amp: f32, a: f32) -> (f32, f32, f32) {
+    let r2 = powf(x, 2) + powf(y, 2) + powf(z, 2);
+    let r = sqrtf(r2);
     let ar3 = powf((a + r), 3);
-    let r2 = powf(r, 2);
-    -amp * (1 / (4 * PI)) * ((a + 3 * r) / (r2 * ar3))
+    let a = -amp * (1 / (4 * PI)) * ((a + 3 * r) / (r2 * ar3));
+    let ax = a * (x / r);
+    let ay = a * (y / r);
+    let az = a * (z / r);
+    (ax, ay, az)
 }
-fn miyamoto_nagai_force(R: f32, z: f32, amp: f32, a: f32, b: f32) -> (f32, f32) {
+fn miyamoto_nagai_force(x: f32, y: f32, z: f32, amp: f32, a: f32, b: f32) -> (f32, f32, f32) {
+    let R2 = powf(x, 2) + powf(y, 2);
+    let R = sqrtf(R2);
     let z2 = powf(z, 2);
     let b2 = powf(b, 2);
     let sqrtz2b2 = sqrtf(z2 + b2);
     let pyth = powf(a + sqrtz2b2, 2);
-    let R2 = powf(R, 2);
     let denom = powf(pyth + R2, 3 / 2);
-    amp * (R / denom, z * (a + sqrtz2b2) / (sqrtz2b2 * denom))
+    let aR = amp * (R / denom);
+    let ax = aR * (x / R);
+    let ay = aR * (y / R);
+    let az = z * (a + sqrtz2b2) / (sqrtz2b2 * denom);
+    (ax, ay, az)
 }
